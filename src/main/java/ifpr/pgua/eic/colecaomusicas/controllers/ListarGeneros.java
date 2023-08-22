c8fab1544c50790a9e5e509da904892a4395ac14
Hugo Perlin <hugo.perlin@ifpr.edu.br>
2023-08-21 22:29:06 -0300

    código da aula 1408

---
+++ src/main/java/ifpr/pgua/eic/colecaomusicas/controllers/ListarGeneros.java
@@ -0,0 +1,17 @@
+package ifpr.pgua.eic.colecaomusicas.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class ListarGeneros {
+
+    @FXML
+    private ListView<?> lstGeneros;
+
+    @FXML
+    void voltar(ActionEvent event) {
+
+    }
+
+}
